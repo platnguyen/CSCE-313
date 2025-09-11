93267ef69e1b247f7928fb6b189cedf6fc66fb9a
Platini Nguyen <plat.nguyen2005@gmail.com>
2025-09-11 14:48:33 +0000

    Made changes to comments

--- Lab_2/shell.cpp
+++ Lab_2/shell.cpp
@@ -12,56 +12,58 @@ int main () {
     char* cmd1[] = {(char*) "ls", (char*) "-al", (char*) "/", nullptr};
     // translates all input from lowercase to uppercase
     char* cmd2[] = {(char*) "tr", (char*) "a-z", (char*) "A-Z", nullptr};
-
-    // TODO: add functionality
     // Create pipe
     int fds[2];
+    // If either pipe() or fork() returns -1, print that it failed
     if (pipe(fds) == -1) {
 	cerr << "Pipe failed\n";
         return 1;
     }
 
-    // Create child to run first command
+    // Creates child to run first command
     pid_t pid_1 = fork();
     if (pid_1 == -1) {
 	cerr << "Fork failed\n";
 	return 1;
     }
     if (pid_1 == 0) {
-        // In child, redirect output to write end of pipe
+        // Redirects output to the write end of the pipe
         dup2(fds[1], STDOUT_FILENO);
-        // Close the read end of the pipe on the child side.
+        // Closes the read end of the pipe (unused)
         close(fds[0]);
-        // In child, execute the command
-	close(fds[1]); //closes the write end
+        // Closes the write end of the pipe
+        close(fds[1]);
+        // Executes the first command
         execvp(cmd1[0],cmd1);
+        // Prints an error message if exec fails
         cerr << "Exec failed\n";
         return 1;
     }
     else {
 	wait(nullptr);
     }
-    // Create another child to run second command
+    // Make another child for the second command
     pid_t pid_2 = fork();
     if (pid_2 == -1) {
 	cerr << "Fork failed\n";
 	return 1;
     }
     if (pid_2 == 0) {
-        // In child, redirect input to the read end of the pipe
+        // Redirects input to the read end of the pipe
         dup2(fds[0], STDIN_FILENO);
-        // Close the write end of the pipe on the child side.
+        // Closes the write end of the pipe (unused)
         close(fds[1]);
-        // Execute the second command.
-	close(fds[0]); //closes the read end
+        // Closes the read end of the pipe
+        close(fds[0]);
+        // Executes the second command.
         execvp(cmd2[0],cmd2);
         cerr << "Exec failed\n";
         return 1;
     }
-    // Reset the input and output file descriptors of the parent.
+    // Closes both ends of the pipe for parent
     close(fds[0]);
     close(fds[1]);
-
+    // Waits for child of the specific pid
     waitpid(pid_1, nullptr, 0);
     waitpid(pid_2, nullptr, 0);
 }
